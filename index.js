e333c38b69afc782b5e9c11a8606ebbba250f311
Mohammed Aliyaan <mohammed007aliyan@gmail.com>
2022-08-30 15:54:00 +0530

    Added Index.js

--- index.js
+++ index.js
@@ -7,16 +7,11 @@ const cors = require('cors');
 app.use(cors());
 app.use(Express.json());
 
-// Okay
 const getRouter = require('./routes/getRouter');
 const postRouter = require('./routes/postRouter');
 
 app.use('/list', getRouter);
 app.use('/todo', postRouter);
-
-app.get('/', (req, res) => {
-    res.send("<div>Hello</div>");
-})
 
 // connecting Database
 mongoose.connect(DB_URL, () => {
